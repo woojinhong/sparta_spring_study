34ad219a87e91c6f6efb3c2c28d5012c176018bc
woojin <orolzleim@gmail.com>
2024-09-01 14:51:58 +0900

    spring security: jwt 로그인

--- spring-auth/src/main/resources/static/js/basic.js
+++ spring-auth/src/main/resources/static/js/basic.js
@@ -3,9 +3,7 @@ let host = 'http://' + window.location.host;
 $(document).ready(function () {
     const auth = getToken();
     if(auth === '') {
-        $('#login-true').show();
-        $('#login-false').hide();
-       // window.location.href = host + "/api/user/login-page";
+        window.location.href = host + "/api/user/login-page";
     } else {
         $('#login-true').show();
         $('#login-false').hide();
