2b4bef5573407ae6fbc13efcb50f0cdfeb515003
woojin <orolzleim@gmail.com>
2024-09-02 23:15:15 +0900

    oneToMany 테스트

--- jpa-advance/src/test/java/com/sparta/jpaadvance/relation/OneToManyTest.java
+++ jpa-advance/src/test/java/com/sparta/jpaadvance/relation/OneToManyTest.java
@@ -11,30 +11,48 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @SpringBootTest
-public class OneToOneTest {
-    @Autowired
-    FoodRepository foodRepository;
+public class OneToManyTest {
+
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    FoodRepository foodRepository;
 
     @Test
-    @Rollback(value = false) // 테스트에서는 @Transactional 에 의해 자동 rollback 됨으로 false 설정해준다.
-    @DisplayName("1대1 단방향 테스트")
+    @Rollback(value = false)
+    @DisplayName("1대N 단방향 테스트")
     void test1() {
-
         User user = new User();
         user.setName("Robbie");
 
-        // 외래 키의 주인인 Food Entity user 필드에 user 객체를 추가해 줍니다.
+        User user2 = new User();
+        user2.setName("Robbert");
+
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-//        food.setUser(user); // 외래 키(연관 관계) 설정
+        food.getUserList().add(user); // 외래 키(연관 관계) 설정
+        food.getUserList().add(user2); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
+        userRepository.save(user2);
         foodRepository.save(food);
+
+        // 추가적인 UPDATE 쿼리 발생을 확인할 수 있습니다.
+    }
+
+    @Test
+    @DisplayName("1대N 조회 테스트")
+    void test2() {
+        Food food = foodRepository.findById(1L).orElse(null);
+
+        for (User user : food.getUserList()){
+            System.out.println("user.getName() = " + user.getName());
+        }
     }
 
 }
