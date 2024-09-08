fddc0a40a8ebb181d83ce7a0f15142d61c5add6a
woojin <orolzleim@gmail.com>
2024-09-08 20:55:43 +0900

    폴더 생성 및 조회 구현

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -0,0 +1,23 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.FolderRequestDto;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import com.sparta.myselectshop.service.FolderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class FolderController {
+
+    private final FolderService folderService;
+    @PostMapping("/folders")
+    public void addFolders(@RequestBody FolderRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        List<String> folderNames = requestDto.getFolderNames();
+        folderService.addFolders(folderNames, userDetails.getUser());
+    }
+}
